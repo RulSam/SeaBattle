90f8ac2c7733f67b19ec6af088d785e07b842d2e
RulSam <114384211+RulSam@users.noreply.github.com>
2022-12-12 10:32:46 +0500

    Ship added

--- sketch.py
+++ sketch.py
@@ -9,3 +9,31 @@ class Dot:
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
+
+class Ship:
+    def __init__(self, bow, l, o):
+        self.bow = bow
+        self.l = l
+        self.o = o
+        self.lives = l
+
+    @property
+    def dots(self):
+        ship_dots = []
+        for i in range(self.l):
+            cur_x = self.bow.x
+            cur_y = self.bow.y
+
+            if self.o == 0:
+                cur_x += i
+
+            elif self.o == 1:
+                cur_y += i
+
+            ship_dots.append(Dot(cur_x, cur_y))
+
+        return ship_dots
+
+    def shooten(self, shot):
+        return shot in self.dots
+
